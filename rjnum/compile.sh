554d95bf743f18e2fbe21c6292245f4810b0dd29
erthalion <9erthalion6@gmail.com>
2016-02-04 02:54:00 +0600

    New text for rjnum

--- rjnum/compile.sh
+++ rjnum/compile.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-FILES="abstract.md intro.md problem.md solution.md results.md biblio.md"
+FILES="abstract.md intro.md problem.md solution.md results.md conclusions.md biblio.md"
 
 pandoc -S --filter pandoc-crossref --bibliography paper.bib --csl=gost.csl -o ${1}.docx ${FILES} -V lang:russian
