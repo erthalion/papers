d19c143933c97db5e97a8cce7979db3614c274c0
erthalion <9erthalion6@gmail.com>
2016-02-20 13:40:58 +0600

    rjnamm modifications

--- rjnum/compile.sh
+++ rjnum/compile.sh
@@ -2,4 +2,4 @@
 
 FILES="abstract.md intro.md problem.md solution.md results.md conclusions.md biblio.md"
 
-pandoc -S --filter pandoc-crossref --bibliography paper.bib --csl=gost.csl -o ${1}.docx ${FILES} -V lang:russian
+pandoc -S --filter pandoc-crossref --bibliography paper.bib --csl=gost.csl -o ${1}.tex ${FILES} -V lang:russian
---
+++ rjnum/convert.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+convert -quality 100 heart_scheme.png eps3:heart_scheme.eps
