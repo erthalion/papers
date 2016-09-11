b496bf6c72bfa1410f597d8be25cec556be7d546
erthalion <9erthalion6@gmail.com>
2016-09-12 00:44:24 +0700

    Add few examples

---
+++ fp_python/examples/generators.py
@@ -0,0 +1,6 @@
+colors = cycle(["red", "green", "blue", "black"])
+data = (
+    {"id": i, "color": colors.next()}
+    for i in range(10)
+)
+next(ifilter(lambda x: x["color"] == "black", data), None)
