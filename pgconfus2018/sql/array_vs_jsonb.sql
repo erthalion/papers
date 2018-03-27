304793232dde270a6dec29bb350dcdb003352705
Dmitrii Dolgov <dmitrii.dolgov@zalando.de>
2018-03-27 20:58:19 +0200

    More slides and examples

---
+++ pgconfus2018/sql/array_vs_jsonb.sql
@@ -0,0 +1,10 @@
+SELECT array[0] FROM some_table;
+SELECT jsonb->0 FROM some_table;
+-- WIP
+SELECT jsonb[0] FROM some_table;
+
+UPDATE some_table SET array[0] = 'new_value';
+UPDATE some_table
+SET jsonb = jsonb_set(jsonb, '{0}', 'new_value');
+-- WIP
+UPDATE some_table SET jsonb[0] 'new_value';
