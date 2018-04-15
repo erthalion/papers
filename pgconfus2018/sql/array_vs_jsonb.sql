feb6c8e5d82c2f486845fa1f5914a730429ba1d3
erthalion <9erthalion6@gmail.com>
2018-04-15 20:07:23 +0200

    Fix typo

--- pgconfus2018/sql/array_vs_jsonb.sql
+++ pgconfus2018/sql/array_vs_jsonb.sql
@@ -7,4 +7,4 @@ UPDATE some_table SET array[0] = 'new_value';
 UPDATE some_table
 SET jsonb = jsonb_set(jsonb, '{0}', 'new_value');
 -- WIP
-UPDATE some_table SET jsonb[0] 'new_value';
+UPDATE some_table SET jsonb[0] = 'new_value';
